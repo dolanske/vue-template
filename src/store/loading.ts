64f3c8c9958842046a8a04ebbe66a6870337819b
Jan Dolanský <dolanovsky@gmail.com>
2023-05-04 21:45:49 +0300

    Update loading.ts
    Added progress property

--- src/store/loading.ts
+++ src/store/loading.ts
@@ -1,44 +1,15 @@
-/**
- * For global state we use pinia. It allows us to access a singele point of truth for data
- * anywhere else in the app
- * 
- * I recommend checking out https://pinia.vuejs.org/core-concepts/
- */
-
 import { defineStore } from 'pinia'
 
 interface State {
   loading: Set<string>
+  progress: number | null
 }
-
-/**
- * This simple store can be used to temporarily store an arbitrary ID.
- * In other parts of the app we can check if this ID is currently being stored here
- * and alter the UI.
- * 
- * Example:
- * 
- * ```
- * loading.add('users')
- * await fetch('/api/users)
- * loading.del('users')
- * ```
- * 
- * In the time the async function ran, the UI can display a loading bar or a spinner.
- * Anywhere in the app we can then use this
- * 
- * ```
- * const loading = useLoading()
- * 
- * // And in the UI we write
- * <Spinner v-if="loading.get('users')" 
- * ```
- */
 
 export const useLoading = defineStore('loading', {
   state: () => ({
-    // We use set instead of an array as it stores only unique values. 
+    // We use set instead of an array as it stores only unique values.
     loading: new Set(),
+    progress: null,
   } as State),
   actions: {
     // Add item(s) to loading
@@ -55,6 +26,21 @@ export const useLoading = defineStore('loading', {
           this.loading.delete(item)
       }
     },
+    // Sets the progress percentage
+    setProgress(amount: number) {
+      if (amount >= 100)
+        this.clearProgress()
+      else
+        this.progress = Math.min(100, amount)
+    },
+    // Completes the progress. Sets progress to 100 and after a slight
+    // delay resets it to null
+    clearProgress() {
+      this.progress = 100
+      setTimeout(() => {
+        this.progress = null
+      }, 300)
+    },
   },
   getters: {
     // Check wether an item is currently loading
@@ -63,7 +49,6 @@ export const useLoading = defineStore('loading', {
         (...items: Array<string>) => {
           if (items.length > 0)
             return Array.from(state.loading).some((item: any) => items.includes(item))
-
           return state.loading.size > 0
         },
   },
